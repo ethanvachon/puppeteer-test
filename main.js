a375e71eb75ee8ebeade988bcb4cb1d59b323bcd
ethanvachon <ethanhvachon@gmail.com>
2021-04-21 16:36:41 -0600

    page .click and typing working

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://google.com');
+  await page.click('.gLFyf')
+  await page.keyboard.type("test")
   await page.screenshot({ path: 'example.png' });
-
   await browser.close();
 })();
